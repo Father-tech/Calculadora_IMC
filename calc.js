10b0120c1e8e7d596aa46ab254246ec9b6fda18b
Father-tech <Eduardosanqueta@hotmail.com>
2024-02-10 19:11:14 -0300

    algumas mudanças no layout mobile!
    foi feita a adição do touch para que o cálculo tambem funcione nos celulares e tablets!

--- calc.js
+++ calc.js
@@ -1,18 +1,29 @@
- function calcular(){
+document.getElementById("botao").addEventListener('click',function calcular(){
     let a = document.getElementById('ialt')
     let p = document.getElementById('ipeso')
     let res = document.getElementById('res')
-    
-    if(a.value && p.value > 0 ){
-        let altvalor = Number(a.value)
-        let alt = altvalor < 3 ? altvalor : altvalor / 100
-        let peso = Number(p.value)
-          
-        let soma = peso / (alt*alt)
 
-        res.innerHTML = `Seu IMC é ${soma.toFixed(2)}`
-    }else {
-        alert('[ERRO] Faltam informações!')
+    var altvalor = Number(a.value)
+    var alt = altvalor < 3 ? altvalor : altvalor / 100
+    var peso = Number(p.value)
+          
+    var soma = peso / (alt*alt)
+
+    
+    if(a.value && p.value <= 0 ){
+        alert('[ERRO] Faltam informações!')    
+    }else if(soma <= 18.5 ){
+        res.innerHTML = `Seu IMC é ${soma.toFixed(2)}. Você está abaixo do peso ideal!`
+    }else if(soma <= 24.9){
+        res.innerHTML = `Seu IMC é ${soma.toFixed(2)}. Você com o peso ideal (parabéns \u{1F642})!`
+    }else if(soma <= 29.9){
+        res.innerHTML = `Seu IMC é ${soma.toFixed(2)}. Você está levemente acima do seu peso ideal!`
+    }else if (soma <= 34.9){
+        res.innerHTML = `Seu IMC é ${soma.toFixed(2)}. Você está com obesidade de grau 1!`
+    }else if(soma <= 39.9){
+        res.innerHTML = `Seu IMC é ${soma.toFixed(2)}. Você está com obesidade de grau 2 (severa)!`
+    }else{
+        res.innerHTML = `Seu IMC é ${soma.toFixed(2)}. Você está com obesidade de gráu 3 (mórbida)!`
     }
 
- }
+})
